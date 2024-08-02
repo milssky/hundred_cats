20707ffce726c7db8e96af38db9b7cb33b008869
Alexey Potapov <2furius@gmail.com>
2024-08-02 14:37:36 +0300

    first commit

---
+++ sync_cats.py
@@ -0,0 +1,53 @@
+import os
+from datetime import datetime
+from pathlib import Path
+
+import requests
+
+BASE_DIR = Path(__file__).parent
+URL = 'https://api.thecatapi.com/v1/images/search'
+CATS_DIR = CATS_DIR = BASE_DIR / 'cats'
+
+
+def get_new_image():
+    response = requests.get(URL).json()
+    random_cat = response[0].get('url')
+    return random_cat
+
+
+def download_file(url):
+    filename = url.split('/')[-1]
+    response = requests.get(url)
+    response.raise_for_status()  # Проверка, что запрос выполнен успешно. Нужно ли?
+    with open(CATS_DIR / filename, 'wb') as file:
+        file.write(response.content)
+
+
+def download_new_cat_image():
+    url = get_new_image()
+    download_file(url)
+
+
+def create_dir(dir_name): 
+    os.makedirs(dir_name, exist_ok=True)
+
+
+def list_dir(dir_name):
+    print(
+        *os.listdir(dir_name),
+        sep='\n'
+    )
+
+
+def main():
+    create_dir(CATS_DIR)
+    for _ in range(100):
+        download_new_cat_image()
+
+
+if __name__ == '__main__':
+    start_time = datetime.now()
+    main()
+    end_time = datetime.now()
+    print(f'Итоговое время выполнения: {end_time - start_time} секунд.')
+    list_dir(CATS_DIR)
